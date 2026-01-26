e68a8a8aa68d269e0af8396d15dd16761d9c239d
DigvijayLohar <digvijaylohar1007@gmail.com>
2026-01-26 17:52:58 +0530

    Added Intial Dotnet Structure

---
+++ Restapis/MedicalRecordsService/MedicalRecordsService/Models/Doctor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicalRecordsService.Models;
+
+public partial class Doctor
+{
+    public int DoctorId { get; set; }
+
+    public int Uid { get; set; }
+
+    public int SpecializationId { get; set; }
+
+    public string BaseQualification { get; set; } = null!;
+
+    public string? PostQualification { get; set; }
+
+    /// <summary>
+    /// Years
+    /// </summary>
+    public int? Experience { get; set; }
+
+    public decimal ConsultationFee { get; set; }
+
+    public virtual ICollection<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+    public virtual ICollection<DoctorAvailability> DoctorAvailabilities { get; set; } = new List<DoctorAvailability>();
+
+    public virtual Specialization Specialization { get; set; } = null!;
+
+    public virtual User UidNavigation { get; set; } = null!;
+}
---
+++ Restapis/MedicalRecordsService/MedicalRecordsService/Models/History.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MedicalRecordsService.Models;
+
+public partial class History
+{
+    public int HistoryId { get; set; }
+
+    public int PatientId { get; set; }
+
+    public string DiseaseName { get; set; } = null!;
+
+    public DateOnly StartDate { get; set; }
+
+    public virtual Patient Patient { get; set; } = null!;
+}
